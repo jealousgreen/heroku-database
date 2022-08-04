3592b6df02d6ffa85b60d483f3e1839c117d852e
jealousgreen <102552821+jealousgreen@users.noreply.github.com>
2022-08-04 16:08:03 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "5199851880:AAFIckiS-jjW58iXyT9uIMpw8fZyhpyRa7Y"
+APP_URL = "https://dilarabot.herokuapp.com/" + BOT_TOKEN
+DB_URI = "postgres://nvydahcrvpiplv:5286f14a4242fb87ed9ced540570fa682ab3897b53fce171a3e99b9386631248@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d5kjj6vkoj0jt"

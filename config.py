63190182ad70265dfd10f257e4432848828ca916
jealousgreen <102552821+jealousgreen@users.noreply.github.com>
2022-08-08 16:14:03 +0300

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-BOT_TOKEN = "5199851880:AAFIckiS-jjW58iXyT9uIMpw8fZyhpyRa7Y"
-APP_URL = "https://heroku-telebot-app.herokuapp.com/" + BOT_TOKEN
-DB_URI = "postgres://mamwepovugzecw:2a60758843338fc32e1f4cbaaaa998bd0674c9e2d347c2ff7026c8e639a37c0c@ec2-34-253-119-24.eu-west-1.compute.amazonaws.com:5432/dehodsut3gsk5c"
